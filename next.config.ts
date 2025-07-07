4b2be85f19416a4171b859abafd2b8ccc0c47d87
bekacholokava <bcholokava@gmail.com>
2025-07-08 00:46:30 +0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "webtrader.salesvault.dev",
+        "localhost:3000",
+        "localhost:7699",
+      ],
+    },
+  },
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
